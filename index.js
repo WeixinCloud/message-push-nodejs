5040da578d6a167da3b5799d99aafb3827fae7ee
Weijia Wang <starkewang@tencent.com>
2022-01-18 18:26:33 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
+const fs = require('fs')
 
 const PORT = process.env.PORT || 80
 const HOST = '0.0.0.0'
 
-// App
 const app = express()
 
 app.use(bodyParser.raw())
@@ -14,22 +14,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const client = axios.default
 
-app.all('/', async (req, res) => {
+const indexPage = fs.readFileSync('index.html')
+
+app.get('/', async (req, res) => {
+    res.send(indexPage)
+})
+
+app.post('/', async (req, res) => {
     console.log('==========')
     console.log('收到消息：')
     console.log(req.body)
     console.log('==========')
 
-    const headers = req.headers
+    // 免鉴权发送消息
     const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`
     const payload = {
-        touser: headers['x-wx-openid'],
+        touser: req.headers['x-wx-openid'],
         msgtype: 'text',
         text: {
             content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
         }
     }
-    // dispatch to wx server
     const result = await client.post(weixinAPI, payload)
 
     console.log('==========')
